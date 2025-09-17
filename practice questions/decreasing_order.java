7a4728826f1f645500e9824b95e3a4d1a25c43a4
Shiksha Yadav <shikshyadav81@gmail.com>
2025-09-17 16:39:04 +0545

    tic tac

---
+++ practice questions/decreasing_order.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class decreasing_order {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i = x; i>=0; i--){
+            System.err.print(i + " ");
+        }
+    }
+}
